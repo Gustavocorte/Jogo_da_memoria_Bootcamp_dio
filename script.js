874da79a44119ca1468a29a2bea46d3c22e26655
GustavoCortes <gustavocortes981@gmail.com>
2022-05-28 19:20:10 -0300

    Jogo da Memoria

---
+++ script.js
@@ -0,0 +1,88 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+let tentativas = 0;
+
+
+//função para virar carta
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    
+    tentativas++;
+    document.getElementById("total").innerHTML = tentativas.toString();
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+    
+}
+
+//função que checa se as cartas são iguais
+function checkForMatch() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+//função que desabilita as cartas
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+//funcão que desvira as cartas
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+//função que reseta o tabuleiro
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//função que embaralha as cartas
+(function shuffle() {
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
+
+//adiciona evento de clique na carta
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
+
+
+function jogarNovamente() {
+    document.location.reload(true);
+  }
+  
+  function desistir() {
+    document.getElementsByTagName("h2")[0].innerHTML = "Você desistiu!";
+    for (let i = 0; i < cards.length; ++i) {
+      cards[i].removeEventListener("click", flipCard);
+      cards[i].classList.add("flip");
+    }
+  }
